881e72c6befc901f4b94079cd0e0c9775548fa1c
Mehedi <mehedi.hasan.tsx@gmail.com>
2026-01-31 18:44:53 +0600

    feat: Add initial Next.js page, global styles, a new CTA component, and related assets, along with VS Code settings.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "@/components/home/hero/Hero";
 import WhatWeDo from "@/components/home/what-we-do/WhatWeDo";
+import CTA from "@/components/home/cta/CTA";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <WhatWeDo />
+      <CTA />
     </>
   );
 };
