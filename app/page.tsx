0351132675088e07b1972b1e64d4f70166d31db7
abirdoesfrontend <abirkhan.official.tsx@gmail.com>
2026-02-04 09:59:14 +0600

    fix: build error

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 import StatsSection from "@/components/home/stats/StatsSection";
 import DominanceSection from "@/components/home/dominance/Dominance";
-import EmpoweringSection from "@/components/home/empowering/EmpoweringSection";
+import EmpoweringSection from "../components/home/empowering/EmpoweringSection";
 const HeroImage = "/hero/hero-big.png";
 
 const Home = () => {
